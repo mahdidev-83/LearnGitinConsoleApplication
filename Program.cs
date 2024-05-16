afeb9dc99aff5db272a02cf6236bb6d211ead8ba
Mantounio <mahdidev83@gmail.com>
2024-05-16 19:10:09 +0330

    The First commit for printing hello world!

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LearnGitinConsoleapplication
+{
+    public class LearnGitinConsoleapplication
+    {
+        public static void Main(String[] args)
+        {
+            string String1 = "Hello World";
+            Console.WriteLine(String1);
+            Console.ReadLine();
+        }
+
+    }
+}
