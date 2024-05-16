393d16d8a06d70c37406660c093493f72910e10d
Mantounio <mahdidev83@gmail.com>
2024-05-16 19:26:47 +0330

    we added the sum of two numbers in SUM branch!

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ namespace LearnGitinConsoleapplication
         {
             string String1 = "Hello World";
             Console.WriteLine(String1);
+            
+
+            // the sum of two numbers:
+            Console.WriteLine(sum(1, 2)); // the sum of 1 + 2 = 3;
             Console.ReadLine();
         }
 
@@ -16,5 +20,11 @@ namespace LearnGitinConsoleapplication
             return $"my name is {name}";
         }
 
+        // the sum of two numbers in the program.cs but in the (SUM) branch!
+        public static int sum(int m, int n)
+        {
+            return m + n;
+        }
+        
     }
 }
